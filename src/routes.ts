e94d357f5a4f6b8e66e42cc72e3ac2bea3eb9034
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-02-12 17:01:16 -0300

    typing mixed arrays

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,12 @@ import { Request, Response } from 'express';
 import createUser from './services/CreateUser'
 
 export function a(req: Request, res: Response) {
-  const user = createUser({email: 'dapscreed@gmail.com', password: 'coxinha2004'});
+  const user = createUser({email: 'dapscreed@gmail.com', password: 'coxinha2004', techs: [
+    'Pao',
+    'Melancia',
+    'Maca',
+    { nome: 'Banana', data: '14/10/2021' }
+  ]});
 
   return res.json(user);
 }
--- src/services/CreateUser.ts
+++ src/services/CreateUser.ts
@@ -2,6 +2,7 @@ interface UserData {
   name?: string,
   email: string,
   password: string,
+  techs: Array<string | object>,
 }
 
 export default function createUser({ name, email, password }: UserData) {
